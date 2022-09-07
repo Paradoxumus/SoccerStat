532caef5ad2a38ef79c9ce4790549a9f82b5386f
Paradoxumus <ser-egorka@yandex.ru>
2022-09-07 23:59:21 +0400

    Create

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import {Box, Tab} from '@mui/material';
+import {TabList, TabContext, TabPanel} from '@mui/lab';
+import { maxWidth } from '@mui/system';
+
 
 function App() {
+  const [value, setValue] = React.useState('1');
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TabContext value={value}>
+        <Box sx ={{borderBottom: 1, borderColor: 'divider',width: maxWidth , height: 80}}>
+          <TabList onChange={handleChange}>
+          <Box
+          sx={{
+            width: 80,
+            height: 80,
+            marginLeft: 1
+          }}
+          />
+            <Tab label="Лиги" value="1"/>
+            <Tab label="Команды" value="2"/>
+          </TabList>
+        </Box>
+      </TabContext>
     </div>
   );
 }
